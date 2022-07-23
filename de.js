1387a4c4228b4508ba0f4d5c32d5eb3cfd19e744
CSAK04 <pgmak9975@gmail.com>
2022-07-23 22:20:43 +0530

    Update de.js

--- de.js
+++ de.js
@@ -1,6 +1,7 @@
 quiz = [
   {Q:"Four identical particles of equal masses 1kg made to move along the circumference of a circle of radius 1 m under the action of their own mutual gravitational attraction. The speed of each particle will be",
-   A: "HLLO", },
+   A: "HLLO",
+   B:"HAI",},
 ]
 
 quizno = 0
@@ -8,6 +9,9 @@ const answers= document.querySelectorAll(".answer")/*name*/
 const submitbtn = document.getElementById("submit")
 const q = document.getElementById('q11')
 const a_opt = document.getElementById("a_text")
+const b_opt = document.getElementById("b_text")
+const c_opt = document.getElementById("c_text")
+const d_opt = document.getElementById("d_text")
 
 /* for deselecting */
 const deselect = () => {
@@ -26,6 +30,9 @@ const select = () => {
 const loadQuiz = () => {
   const aquiz = quiz[quizno];
   a_opt.innerText = aquiz.A;
+  b_opt.innerText = aquiz.B;
+  c_opt.innerText = aquiz.C;
+  d_opt.innerText = aquiz.D;
 };
 
 loadQuiz();
